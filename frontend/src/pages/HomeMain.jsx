4b1ea2883d23a7f777c196797bc2523d34779532
jathursi <jathukirubajathu@gmail.com>
2025-03-03 19:05:21 +0530

    update

---
+++ frontend/src/pages/HomeMain.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function HomeMain() {
+  return (
+    <div className='text-white flex items-center justify-center h-[500px]'>
+        <header className='flex justify-center '>
+            <div className='flex flex-col justify-center '>
+                <h2>Welcome To</h2>
+                <h1 className='text-center' >Squid Proxy</h1>
+                <p> Content Filtering and Web Traffic Management.</p>
+                <div className='d-flex flex-column flex-sm-row align-items-center'> 
+                </div>
+            </div>
+        </header>
+        <div>
+            <div>        
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomeMain
